9442c00c535db6b2d960f000e7d7f39a6607b9ee
nawm77 <alpha050603@gmail.com>
2023-10-27 20:45:25 +0300

    add model controllers

--- src/main/java/ru/ilya/lab2_spring/controller/v1/brand/BrandControllerImpl.java
+++ src/main/java/ru/ilya/lab2_spring/controller/v1/brand/BrandControllerImpl.java
@@ -4,10 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-import ru.ilya.lab2_spring.controller.v1.BrandControllerBase;
 import ru.ilya.lab2_spring.dto.BrandDTO;
 import ru.ilya.lab2_spring.service.BrandService;
-import ru.ilya.lab2_spring.service.util.ValidationUtil;
 import ru.ilya.lab2_spring.util.exception.IllegalArgumentRequestException;
 
 import java.util.List;
@@ -17,8 +15,8 @@ import java.util.List;
 public class BrandControllerImpl extends BrandControllerBase implements BrandController{
 
     @Autowired
-    public BrandControllerImpl(BrandService brandService, ValidationUtil validationUtil) {
-        super(brandService, validationUtil);
+    public BrandControllerImpl(BrandService brandService) {
+        super(brandService);
     }
 
     @Override
---
+++ src/main/java/ru/ilya/lab2_spring/controller/v1/model/ModelControllerImpl.java
@@ -0,0 +1,4 @@
+package ru.ilya.lab2_spring.controller.v1.model;
+
+public class ModelControllerImpl implements ModelController{
+}
